a64c30e7dc62396b4921e2d34f98363f2bb722ae
Prashant Gaikwad <cg316612@gmail.com>
2026-08-26 23:07:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelector(".nav-links");
+
+menu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
